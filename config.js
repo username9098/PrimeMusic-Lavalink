fdf48e6da83da2767464a8dd3aae111ae83add05
username9098 <toufikkdjelal@gmail.com>
2025-07-27 15:06:12 +0100

    Update config.js
    music

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://HeXel:<db_password>@cluster0.svepm0k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
